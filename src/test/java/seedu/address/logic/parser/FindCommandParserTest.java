c4ef33bbe8d09bcf4579e00aa047972d2dd83ace
rotarykirby <e1406360@u.nus.edu>
2025-10-10 01:01:16 +0800

    Add more tests to increase coverage
    Add more tests to increase coverage for FindCommandParser

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -1,15 +1,19 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.PersonMatchesKeywordsPredicate;
 
 public class FindCommandParserTest {
 
@@ -31,4 +35,31 @@ public class FindCommandParserTest {
         assertParseSuccess(parser, " \n Alice \n \t Bob  \t", expectedFindCommand);
     }
 
+    @Test
+    public void parse_singleKeyword_returnsFindCommand() {
+        FindCommand expectedFindCommand =
+                new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("Alice")));
+        assertParseSuccess(parser, "Alice", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validNamePrefix_returnsFindCommand() {
+        FindCommand expectedFindCommand = new FindCommand(
+                new PersonMatchesKeywordsPredicate(Arrays.asList("Alice", "Bob"), List.of()));
+        assertParseSuccess(parser, " n:Alice Bob", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validTagPrefix_returnsFindCommand() {
+        FindCommand expectedFindCommand = new FindCommand(
+                new PersonMatchesKeywordsPredicate(List.of(), Arrays.asList("big-spender", "colleague")));
+        assertParseSuccess(parser, " t:big-spender colleague", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validBothPrefixes_returnsFindCommand() {
+        FindCommand expectedFindCommand = new FindCommand(
+                new PersonMatchesKeywordsPredicate(Arrays.asList("Alice"), Arrays.asList("colleague")));
+        assertParseSuccess(parser, " n:Alice t:colleague", expectedFindCommand);
+    }
 }
