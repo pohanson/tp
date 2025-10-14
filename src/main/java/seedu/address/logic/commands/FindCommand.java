92cbe0f5099fa2ca7782e92c8d2ef1750aa5d173
rotarykirby <e1406360@u.nus.edu>
2025-10-14 19:48:09 +0800

    Change all old tag syntax instances
    Change all old tag syntax usage instances into the new syntax. This
    includes changing all the syntax of test cases and user documents.

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -21,9 +21,9 @@ public class FindCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose name or tag matches any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: n/[name] t/[tag] ..\n"
-            + "Example: " + COMMAND_WORD + " n/alice"
-            + "Example: " + COMMAND_WORD + " t/free"
+            + "Parameters: n:[name] t:[tag] ..\n"
+            + "Example: " + COMMAND_WORD + " " + PREFIX_NAME + "alice\n"
+            + "Example: " + COMMAND_WORD + " " + PREFIX_TAG + "free\n"
             + "Parameters: "
             + "[" + PREFIX_NAME + "NAME] "
             + "[" + PREFIX_TAG + "TAG]...\n"
