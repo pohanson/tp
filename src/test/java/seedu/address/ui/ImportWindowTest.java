3a385999fefddf97fdae8bb560ca98a516198025
PohAnson <50758680+pohanson@users.noreply.github.com>
2025-10-15 23:14:21 +0800

    Try to make it work for CI/CD ImportWindow

--- src/test/java/seedu/address/ui/ImportWindowTest.java
+++ src/test/java/seedu/address/ui/ImportWindowTest.java
@@ -3,8 +3,10 @@ package seedu.address.ui;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationTest;
 
+import javafx.application.Platform;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import seedu.address.logic.Logic;
@@ -22,15 +24,17 @@ public class ImportWindowTest extends ApplicationTest {
 
     private ImportWindow importWindow;
     private Logic logic;
+    private Stage stage;
 
 
     @Override
     public void start(Stage stage) throws Exception {
+        this.stage = stage;
         // Set up test dependencies
-        JsonAddressBookStorage addressBookStorage = new JsonAddressBookStorage(
-                java.nio.file.Paths.get("test", "data", "temp.json"));
-        JsonUserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(
-                java.nio.file.Paths.get("test", "data", "tempPrefs.json"));
+        JsonAddressBookStorage addressBookStorage =
+                new JsonAddressBookStorage(java.nio.file.Paths.get("test", "data", "temp.json"));
+        JsonUserPrefsStorage userPrefsStorage =
+                new JsonUserPrefsStorage(java.nio.file.Paths.get("test", "data", "tempPrefs.json"));
         StorageManager storage = new StorageManager(addressBookStorage, userPrefsStorage);
         Model model = new ModelManager();
         logic = new LogicManager(model, storage);
@@ -43,7 +47,10 @@ public class ImportWindowTest extends ApplicationTest {
 
     @Test
     public void display_initialText_showsDefaultPreview() {
-        Text preview = lookup("#jsonPreview").query();
-        assertEquals("No data yet.\nPaste JSON data here", preview.getText());
+        Platform.runLater(() -> {
+            FxRobot robo = new FxRobot();
+            Text preview = robo.from(stage.getScene().getRoot()).lookup("#jsonPreview").queryText();
+            assertEquals("No data yet.\nPaste JSON data here", preview.getText());
+        });
     }
 }
