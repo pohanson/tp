fa1632bee0e3025ba86b76a3dd10f18d81878fca
Nihaal Manaf <nihaalmanaf@gmail.com>
2025-10-15 12:50:42 +0800

    Add testing for statusview

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -14,6 +14,7 @@ import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
 
+import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.collections.ObservableList;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.logic.Messages;
@@ -22,6 +23,7 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
+import seedu.address.model.StatusViewState;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
 
@@ -157,6 +159,16 @@ public class AddCommandTest {
         public void updateFilteredPersonList(Predicate<Person> predicate) {
             throw new AssertionError("This method should not be called.");
         }
+
+        @Override
+        public ReadOnlyObjectProperty<StatusViewState> getStatusViewStateProperty() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setStatusViewState(StatusViewState state) {
+            throw new AssertionError("This method should not be called.");
+        }
     }
 
     /**
@@ -198,6 +210,11 @@ public class AddCommandTest {
         @Override
         public ReadOnlyAddressBook getAddressBook() {
             return new AddressBook();
+        }
+
+        @Override
+        public void setStatusViewState(StatusViewState state) {
+            // Do nothing - stub implementation
         }
     }
 
