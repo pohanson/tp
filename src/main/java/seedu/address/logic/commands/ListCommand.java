f1d37af8207998deca86b95ad6658dcd1b0fa542
Nihaal Manaf <nihaalmanaf@gmail.com>
2025-10-15 12:44:38 +0800

    Implement set state view for trivial commands like add to show all states

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -4,6 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import seedu.address.model.Model;
+import seedu.address.model.StatusViewState;
 
 /**
  * Lists all persons in the address book to the user.
@@ -19,6 +20,7 @@ public class ListCommand extends Command {
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        model.setStatusViewState(StatusViewState.ALL_STATUSES);
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
